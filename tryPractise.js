38f8f17ec34244f3bd4a58a6457767ca18f34660
fullstacksherpa <fullstacksherpa@gmail.com>
2024-01-28 22:51:58 -0800

    feat: added try

---
+++ tryPractise.js
@@ -0,0 +1,28 @@
+{/*
+
+
+Platform module Platform specific file extensions
+Platform module, platform specific file extensions
+Platform module, Platform specific file extensions
+height: Platform.OS === 'ios' ? 200: 100;
+height: Platform.OS === 'ios' ? 200: 100;
+
+all of the core component accept prop named style.
+all fo the core component accept prop named syle.
+all of the core component accept prop name style.
+all of the core component accept prop name style.
+backgroundColor:
+StyleSheet.creat({
+    
+})
+
+
+
+
+
+
+
+
+
+
+*/}
